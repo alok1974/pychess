a4a77d2261d1a8a26b091a974917b77215b55032
Alok Gandhi <alok.gandhi2002@gmail.com>
2020-09-01 15:24:17 +0530

    Initial implementation of gameplay

--- src/pychess/gamer.py
+++ src/pychess/gamer.py
@@ -9,6 +9,10 @@ class Game:
         self._captured_white = []
         self._captured_black = []
         self._move_history = []
+        self._capturables = []
+        self._pieces_checking_black = []
+        self._pieces_checking_white = []
+        self._color_to_move = Color.white
 
     @property
     def board(self):
@@ -43,43 +47,38 @@ class Game:
     def lead(self):
         return abs(self.black_points - self.white_points)
 
+    @property
+    def pieces_checking_black(self):
+        return self._pieces_checking_black
+
+    @property
+    def pieces_checking_white(self):
+        return self._pieces_checking_white
+
+    @property
+    def move_history(self):
+        return self._move_history
+
+    @property
+    def capturables(self):
+        return self._capturables
+
     def reset(self):
         self.board.clear()
+        self._captured_white = []
+        self._captured_black = []
+        self._move_history = []
+        self._capturables = []
+        self._pieces_checking_black = []
+        self._pieces_checking_white = []
+        self._color_to_move = Color.white
 
     def is_game_over(self):
         pass
 
     def move(self, src, dst):
-        piece_to_move = self.board.get_piece(src)
-
-        # Case I: No piece to move
-        if piece_to_move is None:
-            print('piece to move is None')
+        if not self._is_move_legal(src, dst):
             return
-
-        # Case II: Illegal move for piece
-        mv = Move(piece_to_move, src, dst)
-        if not mv.is_legal:
-            print('move is illegal')
-            return
-
-        # Case III: Destination has piece of same color
-        dst_piece = self.board.get_piece(dst)
-        if dst_piece is not None:
-            if piece_to_move.color == dst_piece.color:
-                print('trying to capture piece of same color')
-                return
-
-        # Case IV: Path to destination is not empty
-        if piece_to_move.type in [
-                PieceType.queen,
-                PieceType.rook,
-                PieceType.rook,
-        ]:
-            in_between_squares = mv.path[1:-1]
-            if not all([self.board.is_empty(s) for s in in_between_squares]):
-                print('path is obstructed')
-                return
 
         dst_piece = self.board.clear_square(dst)
         if dst_piece is not None:
@@ -90,3 +89,109 @@ class Game:
 
         src_piece = self.board.clear_square(src)
         self.board.add_piece(src_piece, dst)
+
+        self._get_capturables()
+        self._move_history.append(Move(src_piece, src, dst))
+        self._change_color_to_move()
+
+    def _change_color_to_move(self):
+        if self._color_to_move == Color.black:
+            self._color_to_move = Color.white
+        else:
+            self._color_to_move = Color.black
+
+    def _is_move_legal(self, src, dst, check_player=True):
+        piece_to_move = self.board.get_piece(src)
+
+        # Case I: No piece to move
+        if piece_to_move is None:
+            return False
+
+        # Case II: Not this player's turn
+        if check_player:
+            if piece_to_move.color != self._color_to_move:
+                return False
+
+        # Case III: Illegal move for piece
+        mv = Move(piece_to_move, src, dst)
+        if not mv.is_legal:
+            return False
+
+        # Case IV: Destination has piece of same color
+        dst_piece = self.board.get_piece(dst)
+        if dst_piece is not None:
+            if piece_to_move.color == dst_piece.color:
+                return False
+
+        # Case V: Special case for pawn as it can only capture only
+        # with a diagonal move
+        if piece_to_move.type == PieceType.pawn:
+            if mv.is_orthogonal and dst_piece is not None:
+                return
+            elif mv.is_diagonal and dst_piece is None:
+                return
+
+        # Case V: Path to destination is not empty
+        if piece_to_move.type in [
+                PieceType.queen,
+                PieceType.rook,
+                PieceType.bishop,
+        ]:
+            in_between_squares = mv.path[1:-1]
+            if not all([self.board.is_empty(s) for s in in_between_squares]):
+                return False
+
+        return True
+
+    def _get_capturables(self):
+        self._capturables = {
+            Color.white: {},
+            Color.black: {},
+        }
+        for color in [Color.white, Color.black]:
+            threatening_color = (
+                Color.black
+                if color == Color.white
+                else Color.white
+            )
+
+            threatening_pieces = self._get_pieces_on_board(threatening_color)
+            threatened_pieces = self._get_pieces_on_board(color)
+
+            for threatening_piece in threatening_pieces:
+                for threatened_piece in threatened_pieces:
+                    src = self.board.get_square(threatening_piece)
+                    dst = self.board.get_square(threatened_piece)
+                    if self._is_move_legal(src, dst, check_player=False):
+                        self._capturables[color].setdefault(
+                            threatening_piece, []
+                        ).append(threatened_piece)
+
+                        # look for checks
+                        if threatened_piece.type == PieceType.king:
+                            if threatened_piece.color == Color.white:
+                                self._pieces_checking_white.append(
+                                    threatening_piece
+                                )
+                            else:
+                                self._pieces_checking_black.append(
+                                    threatening_piece
+                                )
+
+    def _is_mate(self, color):
+        is_under_check = bool(
+            self._pieces_checking_black
+            if color == Color.black
+            else self._pieces_checking_white
+        )
+
+        if is_under_check:
+            # Case I: Checking pieces can be captured
+            pass
+
+    def _get_pieces_on_board(self, color):
+        return [
+            piece
+            for piece in self.board.pieces
+            if piece.color == color
+        ]
--- src/test/unit/testgamer.py
+++ src/test/unit/testgamer.py
@@ -3,41 +3,170 @@ import unittest
 
 from pychess.gamer import Game
 from pychess.squarer import Square
+from pychess.piecer import Piece
+from pychess.constant import PieceType, Color
+from pychess.mover import Move
 
 
 class TestGamer(unittest.TestCase):
-    def setUp(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        # A simple gameplay (even a noob would not play so bad ;)
+        cls.game = Game()
 
-    def tearDown(self):
-        pass
+        # Illegal Move - White rook trying to move diagonally
+        cls.game.move(Square('a1'), Square('h8'))
 
-    def test_move(self):
-        game = Game()
+        # Move 1 - White opening with d4
+        cls.game.move(Square('d2'), Square('d4'))
 
-        # Illegal move of white rook, will do nothing
-        game.move(Square('a1'), Square('h8'))
+        # Move 2 - Black opening with c6
+        cls.game.move(Square('c7'), Square('c6'))
 
-        game.move(Square('d2'), Square('d4'))
-        game.move(Square('d7'), Square('d5'))
+        # Illegal Move - Trying to capture white pawn at d4 by white queen
+        cls.game.move(Square('d1'), Square('d4'))
 
-        # Illegal move trying to capture white pawn at d4 by white queen
-        # this move will not happen
-        game.move(Square('d1'), Square('d4'))
+        # Move 3 - White Queen to d3
+        cls.game.move(Square('d1'), Square('d3'))
 
-        game.move(Square('d1'), Square('d3'))
-
-        # queen trying to jump over white pawn at d4
+        # Illegal Move - Queen trying to jump over white pawn at d4
         # nothing will happen
-        game.move(Square('d3'), Square('d5'))
+        cls.game.move(Square('d3'), Square('d5'))
 
-        # legal capture of black pawn at h7 by the queen
-        game.move(Square('d3'), Square('h7'))
-        print(game.board)
-        print(game.captured_black)
-        print(game.captured_white)
-        print(game.leader)
-        print(game.lead)
+        # Move 4 - Black moves pawn a6
+        cls.game.move(Square('a7'), Square('a6'))
+
+        # Move 5- Black pawn at h7 captured by the white queen
+        cls.game.move(Square('d3'), Square('h7'))
+
+        # Move 6 - Black pawn to a5
+        cls.game.move(Square('a6'), Square('a5'))
+
+        # Move 7 - White queen to f5
+        cls.game.move(Square('h7'), Square('f5'))
+
+        # Move 8 - Black rook to h6, where it comes under attack
+        # by the black bishop at c1
+        cls.game.move(Square('h8'), Square('h6'))
+
+        # Move 9 - White pawn to e4
+        cls.game.move(Square('e2'), Square('e4'))
+
+        # Move 10 - Black pawn to a4
+        cls.game.move(Square('a5'), Square('a4'))
+
+        # Move 11 - White rook to c4
+        cls.game.move(Square('f1'), Square('c4'))
+
+        # Move 12 - White rook to h5 attacking the queen
+        cls.game.move(Square('h6'), Square('h5'))
+
+        # Move 13 - White queen to f7, it's a mate!
+        cls.game.move(Square('f5'), Square('f7'))
+
+    def test_board(self):
+        expected_result = (
+            '    a b c d e f g h\n'
+            ' 8 |r|n|b|q|k|b|n|#| 8\n'
+            ' 7 |#|p|#|p|p|Q|p|_| 7\n'
+            ' 6 |_|#|p|#|_|#|_|#| 6\n'
+            ' 5 |#|_|#|_|#|_|#|r| 5\n'
+            ' 4 |p|#|B|P|P|#|_|#| 4\n'
+            ' 3 |#|_|#|_|#|_|#|_| 3\n'
+            ' 2 |P|P|P|#|_|P|P|P| 2\n'
+            ' 1 |R|N|B|_|K|_|N|R| 1\n'
+            '    a b c d e f g h'
+        )
+        self.assertEqual(repr(self.game.board), expected_result)
+
+    def test_captured_black(self):
+        expected_result = [
+            Piece(PieceType.pawn, Color.black, order=7),
+            Piece(PieceType.pawn, Color.black, order=5),
+        ]
+
+        self.assertEqual(self.game.captured_black, expected_result)
+
+    def test_captured_white(self):
+        expected_result = []
+        self.assertEqual(self.game.captured_white, expected_result)
+
+    def test_white_points(self):
+        expected_result = 2
+        self.assertEqual(self.game.white_points, expected_result)
+
+    def test_black_points(self):
+        expected_result = 0
+        self.assertEqual(self.game.black_points, expected_result)
+
+    def test_leader(self):
+        expected_result = Color.white
+        self.assertEqual(self.game.leader, expected_result)
+
+    def test_lead(self):
+        expected_result = 2
+        self.assertEqual(self.game.lead, expected_result)
+
+    def test_pieces_checking_black(self):
+        expected_result = [Piece(PieceType.queen, Color.white)]
+        self.assertEqual(self.game.pieces_checking_black, expected_result)
+
+    def test_pieces_checking_white(self):
+        expected_result = []
+        self.assertEqual(self.game.pieces_checking_white, expected_result)
+
+    def test_move_history(self):
+        # Some short hands for fitting one move in one line
+        p = PieceType.pawn
+        b = PieceType.bishop
+        r = PieceType.rook
+        q = PieceType.queen
+
+        wt = Color.white
+        bl = Color.black
+        s = Square
+
+        expected_result = [
+            Move(Piece(p, wt, 3), s('d2'), s('d4')),
+            Move(Piece(p, bl, 2), s('c7'), s('c6')),
+            Move(Piece(q, wt, 0), s('d1'), s('d3')),
+            Move(Piece(p, bl, 0), s('a7'), s('a6')),
+            Move(Piece(q, wt, 0), s('d3'), s('h7')),
+            Move(Piece(p, bl, 0), s('a6'), s('a5')),
+            Move(Piece(q, wt, 0), s('h7'), s('f5')),
+            Move(Piece(r, bl, 1), s('h8'), s('h6')),
+            Move(Piece(p, wt, 4), s('e2'), s('e4')),
+            Move(Piece(p, bl, 0), s('a5'), s('a4')),
+            Move(Piece(b, wt, 1), s('f1'), s('c4')),
+            Move(Piece(r, bl, 1), s('h6'), s('h5')),
+            Move(Piece(q, wt, 0), s('f5'), s('f7')),
+        ]
+
+        self.assertEqual(self.game.move_history, expected_result)
+
+    def test_capturables(self):
+        expected_result = {
+            Color.white: {
+                Piece(PieceType.king, Color.black): [
+                    Piece(PieceType.queen, Color.white)
+                ],
+                Piece(PieceType.rook, Color.black, 1): [
+                    Piece(PieceType.pawn, Color.white, 7)
+                ]
+            },
+            Color.black: {
+                Piece(PieceType.queen, Color.white): [
+                    Piece(PieceType.pawn, Color.black, 4),
+                    Piece(PieceType.king, Color.black),
+                    Piece(PieceType.bishop, Color.black, 1),
+                    Piece(PieceType.pawn, Color.black, 6),
+                    Piece(PieceType.knight, Color.black, 1),
+                    Piece(PieceType.rook, Color.black, 1),
+
+                ]
+            }
+        }
+        self.assertEqual(self.game.capturables, expected_result)
 
 
 if __name__ == '__main__':
