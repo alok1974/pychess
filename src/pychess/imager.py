f5e2b355215cf7ae146f1b61b36355f69fe1a871
Alok Gandhi <alok.gandhi2002@gmail.com>
2020-09-05 12:57:03 +0530

    Second pass at imager.py

--- src/pychess/imager.py
+++ src/pychess/imager.py
@@ -5,20 +5,20 @@ from PIL import Image, ImageQt
 
 
 from . import constant as c
-from .piecer import generate_pieces, get_piece_row_place
 
 
 class BoardImage:
     def __init__(self, board):
-        self.init_image(board=board)
+        self._board = board
+        self.init()
+
+    @property
+    def board(self):
+        return self._board()
 
     @property
     def qt_image(self):
         return ImageQt.ImageQt(self._board_image)
-
-    @property
-    def board(self):
-        return self._board
 
     @property
     def image(self):
@@ -36,8 +36,7 @@ class BoardImage:
     def height(self):
         return self._board_image.height
 
-    def init_image(self, board):
-        self._board = board
+    def init(self):
         self._board_image = Image.new(
             'RGBA',
             (c.IMAGE_WIDTH, c.IMAGE_HEIGHT),
@@ -45,17 +44,22 @@ class BoardImage:
         )
         board_image = Image.open(c.BOARD_IMAGE_FILE_PATH)
         self._board_image.paste(board_image, (0, 0))
-        self._set_pieces()
 
-    def show(self):
-        self._board_image.show()
+        self.update()
 
-    def _set_pieces(self):
-        for piece in generate_pieces():
+    def update(self):
+        self._draw_pieces()
+
+    def show(self, x_res=900, y_res=900):
+        image = self._board_image.resize((x_res, y_res))
+        image.show()
+
+    def _draw_pieces(self):
+        for piece in self._board.pieces:
             image_path = self._get_piece_image_path(piece)
             piece_image = Image.open(image_path)
             piece_image.load()  # needed for alpha comp
-            x, y = self._get_piece_coord(piece)
+            x, y = self._get_coordinates(piece)
             self._board_image.paste(
                 piece_image,
                 (x, y),
@@ -73,34 +77,27 @@ class BoardImage:
         assert(os.path.exists(image_path)), error_msg
         return image_path
 
-    def _get_piece_coord(self, piece):
-        row = self._get_piece_row(piece)
-        place = get_piece_row_place(piece)
+    def _get_coordinates(self, piece):
+        square = self._board.get_square(piece)
+        row, column = square.x, square.y
+
         piece_image_size = (
             c.PAWN_IMAGE_SIZE
             if piece.type == c.PieceType.pawn
             else c.NON_PAWN_IMAGE_SIZE
         )
 
-        x = int(
-            c.BORDER_SIZE + (c.SQUARE_SIZE * place) +
-            ((c.SQUARE_SIZE - piece_image_size) / 2)
+        return (
+            self._get_coordinate(piece_image_size, row),
+            self._get_coordinate(piece_image_size, column, reverse=True),
         )
 
-        y = int(
-            c.BORDER_SIZE + (c.SQUARE_SIZE * (c.SQAURES_IN_A_ROW - 1 - row)) +
-            ((c.SQUARE_SIZE - piece_image_size) / 2)
-        )
-        return x, y
+    def _get_coordinate(self, image_size, row_or_column, reverse=False):
+        position = row_or_column
+        if reverse:
+            position = c.SQAURES_IN_A_ROW - 1 - row_or_column
 
-    def _get_piece_row(self, piece):
-        row = None
-        if piece.color == c.Color.black:
-            row = 6 if piece.type == c.PieceType.pawn else 7
-        elif piece.color == c.Color.white:
-            row = 1 if piece.type == c.PieceType.pawn else 0
-        else:
-            error_msg = (f'Undefined color {piece.color}!')
-            raise ValueError(error_msg)
+        base_offset = c.SQUARE_SIZE * position
+        image_offset = int((c.SQUARE_SIZE - image_size) / 2)
 
-        return row
+        return c.BORDER_SIZE + base_offset + image_offset
