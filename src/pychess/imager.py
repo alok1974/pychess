e179fbbfef04ad657e6ca909d9d25ac87d0bf998
Alok Gandhi <alok.gandhi2002@gmail.com>
2020-09-05 16:38:50 +0530

    Added resize() to imager.py

--- src/pychess/imager.py
+++ src/pychess/imager.py
@@ -8,8 +8,8 @@ from . import constant as C
 
 
 class BoardImage:
-    def __init__(self, board):
-        self.init(board=board)
+    def __init__(self, board, size=C.IMAGE.DEFAULT_SIZE):
+        self.init(board=board, size=size)
 
     @property
     def board(self):
@@ -31,24 +31,44 @@ class BoardImage:
     def height(self):
         return self._board_image.height
 
-    def init(self, board):
+    def init(self, board, size=C.IMAGE.DEFAULT_SIZE):
         self._board = board
+        self.resize(size=size)
+        self.update()
+
+    def resize(self, size=C.IMAGE.DEFAULT_SIZE):
+        if size not in C.IMAGE.SUPPORTED_SIZE:
+            error_msg = (
+                f'Unable to resize image - invalid size {size}, '
+                f'supported sizes are {C.IMAGE.SUPPORTED_SIZE}'
+            )
+            raise RuntimeError(error_msg)
+
+        self._resize_factor = float(size / C.IMAGE.BASE_IMAGE_SIZE)
+        self._square_size = int(C.IMAGE.SQUARE_SIZE * self._resize_factor)
+        self._border_size = int(C.IMAGE.BORDER_SIZE * self._resize_factor)
+        self._non_pawn_image_size = int(
+            C.IMAGE.NON_PAWN_IMAGE_SIZE * self._resize_factor
+        )
+        self._pawn_image_size = int(
+            C.IMAGE.PAWN_IMAGE_SIZE * self._resize_factor
+        )
+        self._init_board_image()
+
+    def update(self):
+        self._board_image.paste(self._base_image, (0, 0))
+        self._draw_pieces()
+
+    def show(self):
+        self._board_image.show()
+
+    def _init_board_image(self):
         self._base_image = self._load_image(C.IMAGE.BOARD_IMAGE_FILE_PATH)
         self._board_image = Image.new(
             'RGBA',
             self._base_image.size,
             color=(0, 0, 0),
         )
-        self._board_image.paste(self._base_image, (0, 0))
-
-        self.update()
-
-    def update(self, board=None):
-        self._board_image.paste(self._base_image, (0, 0))
-        self._draw_pieces()
-
-    def show(self):
-        self._board_image.show()
 
     def _draw_pieces(self):
         for piece in self._board.pieces:
@@ -62,25 +82,14 @@ class BoardImage:
                 mask=piece_image.split()[3],  # needed for alpha tranparency
             )
 
-    def _get_piece_image_path(self, piece):
-        piece_name = piece.type.name
-        color_name = piece.color.name
-        piece_images = getattr(C.IMAGE.PIECE_IMAGE, piece_name)
-        image_name = getattr(piece_images, color_name)
-        image_path = os.path.join(C.IMAGE.IMAGE_DIR, image_name)
-
-        error_msg = f'Image path {image_path} does not exist!'
-        assert(os.path.exists(image_path)), error_msg
-        return image_path
-
     def _get_coordinates(self, piece):
         square = self._board.get_square(piece)
         row, column = square.x, square.y
 
         piece_image_size = (
-            C.IMAGE.PAWN_IMAGE_SIZE
+            self._pawn_image_size
             if piece.type == C.PieceType.pawn
-            else C.IMAGE.NON_PAWN_IMAGE_SIZE
+            else self._non_pawn_image_size
         )
 
         return (
@@ -93,16 +102,28 @@ class BoardImage:
         if reverse:
             position = C.IMAGE.NB_SQUARES - 1 - row_or_column
 
-        base_offset = C.IMAGE.SQUARE_SIZE * position
-        image_offset = int((C.IMAGE.SQUARE_SIZE - image_size) / 2)
+        base_offset = self._square_size * position
+        image_offset = int((self._square_size - image_size) / 2)
 
-        return C.IMAGE.BORDER_SIZE + base_offset + image_offset
+        return self._border_size + base_offset + image_offset
 
     def _load_image(self, image_path):
         image = Image.open(image_path)
         return image.resize(
             (
-                int(image.width * C.IMAGE.RESIZE_FACTOR),
-                int(image.height * C.IMAGE.RESIZE_FACTOR),
+                int(image.width * self._resize_factor),
+                int(image.height * self._resize_factor),
             )
         )
+
+    @staticmethod
+    def _get_piece_image_path(piece):
+        piece_name = piece.type.name
+        color_name = piece.color.name
+        piece_images = getattr(C.IMAGE.PIECE_IMAGE, piece_name)
+        image_name = getattr(piece_images, color_name)
+        image_path = os.path.join(C.IMAGE.IMAGE_DIR, image_name)
+
+        error_msg = f'Image path {image_path} does not exist!'
+        assert(os.path.exists(image_path)), error_msg
+        return image_path
