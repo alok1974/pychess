80f0f1740bb921539bc3c7e7168e423d7b44dc38
Alok Gandhi <alok.gandhi2002@gmail.com>
2020-09-07 10:13:27 +0530

    A move from src to src will not emit signal

--- src/pychess/gui.py
+++ src/pychess/gui.py
@@ -120,6 +120,9 @@ class MainWindow(QtWidgets.QDialog):
             signal = self.MOVE_SIGNAL
 
         if signal is not None and both_cell_selected:
+            if self._first_square == self._second_square:
+                return
+
             move = f'{self._first_square.address}{self._second_square.address}'
             signal.emit(move)
             self._first_square = None
