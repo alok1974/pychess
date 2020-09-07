0004b57b305bae42a7019ffa4bde92f3d99118d9
Alok Gandhi <alok.gandhi2002@gmail.com>
2020-09-07 11:10:49 +0530

    Updated Signal to support multiple incoming connections

--- src/pychess/event.py
+++ src/pychess/event.py
@@ -1,10 +1,11 @@
 class Signal:
     def __init__(self, arg_type=None):
         self._arg_type = arg_type or type(None)
-        self._connected_callback = None
+        self._connected_callbacks = []
 
     def connect(self, callback):
-        self._connected_callback = callback
+        self._validate_callback(callback)
+        self._connected_callbacks.append(callback)
 
     def emit(self, arg=None):
         if not isinstance(arg, self._arg_type):
@@ -13,11 +14,22 @@ class Signal:
             )
             raise TypeError(error_msg)
 
-        if self._connected_callback is not None:
-            if arg is not None:
-                self._connected_callback(arg)
-            else:
-                self._connected_callback()
+        for callback in self._connected_callbacks:
+            if callback is not None:
+                if arg is not None:
+                    callback(arg)
+                else:
+                    callback()
+
+    def disconnect(self, callback):
+        self._validate_callback(callback)
+        if callback in self._connected_callbacks:
+            self._connected_callbacks.remove(callback)
+
+    def _validate_callback(self, callback):
+        if not callable(callback):
+            error_msg = f'Callback {callback} is not a callable!'
+            raise TypeError(error_msg)
 
     def __repr__(self):
         return f'{self.__class__.__name__}'
